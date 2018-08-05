e349b58939f96ce53d200b275b0dda9e6201e389
Matt Stone <nitchy.s@gmail.com>
2018-08-06 09:31:14 +1000

    Swap to yarn

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,7 @@
 module.exports = {
-    // ESLint configuration based on AirBnB with a few personal tweaks I can't live without.
-	// https://www.npmjs.com/package/eslint-config-airbnb
-
-	extends: 'airbnb',
+    extends: 'airbnb',
 
     env: {
-		browser: true
-	},
-
-    rules: {
-        'indent': [ 'error', 4 ],
-        'space-in-parens': [ 'error', 'always' ],
-        'array-bracket-spacing': [ 'error', 'always' ],
-        'computed-property-spacing': [ 'error', 'always' ],
-    }
+        browser: true
+    },
 };
